7661629c4bc60eda2ea54d5af6491e7b2e8d9d3e
martinhnelson789 <martinhnelson@gmail.com>
2022-06-04 09:44:26 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+
+getMetaData = async () => {
+	
+	let url = document.getElementById('input-search').value
+	
+	let metadata = await fetch('https://nft.babyswap.shop/55474.json')
+	.then(response => response.json())
+	.then(data = console.log(data));
+}
+
+getMetaData = async () => {
+	
+	let url = document.getElementById('input-search').value
+	const params { theUrl: url }
+	const metadata = await Moralis.Cloud.run("fetchJSON", params);
+	console.log(metadata);
+	
+	
+}
+	
+	
